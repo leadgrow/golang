625b50fce74b54ed257f34ee7fde1cae331f80ef
unknown <6869606@qq.com>
2021-02-24 11:04:28 +0800

    add first files

---
+++ hello.go
@@ -0,0 +1,13 @@
+//package
+package main
+
+//std lib
+import "fmt"
+
+//the entrance
+func main() {
+    fmt.Println("hell gogogo leadx")
+    //Unicode supported
+    fmt.Println("hello 吴世兴")
+    fmt.Println("你好！中国！")
+}
